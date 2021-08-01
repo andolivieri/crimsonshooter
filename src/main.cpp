2debc3e41df33bb525224dc81e6e81328f4b4c7d
Andrea Olivieri <and.olivieri@gmail.com>
2021-08-01 08:51:24 +0200

    part 5

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,24 @@ int main(int argc, char* argv[])
             false
             );
 
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    uint32_t frameStart;
+    int frameTime;
+
     while(g->running()){
+        frameStart = SDL_GetTicks();
+
         g->handleEvents();
         g->update();
         g->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDelay > frameTime)
+            SDL_Delay(frameDelay - frameTime);
+
     }
 
     g->clean();
